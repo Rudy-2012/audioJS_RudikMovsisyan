4dcb4ee523537217f895a1fc6e1da39be1a18533
Rudy-2012 <rudik.movsisyan@tumo.org>
2026-07-11 13:48:59 +0400

    add music

---
+++ audio.js
@@ -0,0 +1,26 @@
+var data ={
+    title: [
+        "Lose Yourself ",
+        "rickroll",
+        "Billie Eilish - Everything i wanted (acoustic)",
+        "Emin & JONY - kamin"
+    ],
+
+
+
+     song: [
+        "music/Lose Yourself .mp3",
+        "music/rickroll.mp3",
+        "music/Billie Eilish - Everything i wanted (acoustic)",
+        "music/Emin & JONY - kamin.mp3"
+],
+
+
+
+poster:[
+    "https://i.pinimg.com/originals/a6/16/e3/a616e3266f8a5e42d2b6cea2c2c1aa69.gif",
+    "https://media1.tenor.com/m/x8v1oNUOmg4AAAAC/rickroll-roll.gif",
+    "https://giffiles.alphacoders.com/210/210160.gif",
+    "https://i.makeagif.com/media/11-12-2017/ETAHMp.gif"
+]
+}
